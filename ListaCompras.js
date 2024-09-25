c5095f94538a714473a94f7cc8caa17c96a1109c
silvanoruatabelen1 <168794376+silvanoruatabelen1@users.noreply.github.com>
2024-09-25 10:04:26 -0300

    Update ListaCompras.js
    Agrega funcionalidad eliminar item

--- ListaCompras.js
+++ ListaCompras.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
-function ListaCompras({ items }) {
+function ListaCompras({ items, onEliminarItem }) {
   return (
     <div className="lista-compras">
       <ul>
-        {items.map((item, index) => (
-          <li key={index}>{item}</li>
+        {items.map((item) => (
+          <li key={item.id}>
+            {item.texto}
+            <button onClick={() => onEliminarItem(item.id)} className="boton-eliminar">
+              Eliminar
+            </button>
+          </li>
         ))}
       </ul>
     </div>
